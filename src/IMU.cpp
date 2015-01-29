3c0885961404a94d868687d367194cefe99ca6c1
Stephane Desneux <stephane.desneux@open.eurogiciel.org>
2015-01-29 19:52:04 +0100

    send info/error messages on stderr instead of stdout
    Change-Id: Ifefaddb4cda360b6f6c0f2abaae0747bd5952fc1
    Signed-off-by: Stephane Desneux <stephane.desneux@open.eurogiciel.org>

--- src/IMU.cpp
+++ src/IMU.cpp
@@ -13,35 +13,35 @@ IMU::IMU()
 
 IMU::~IMU()
 {
-	
+
 }
 
 bool IMU::imu_init()
 {
 	m_mutexIMU = PTHREAD_MUTEX_INITIALIZER;
-	
+
 	// default warning lvl values
 	imu_low_warn = 20;
 	imu_strong_warn = 45;
-	
-	// Initialize IMU 
+
+	// Initialize IMU
 	imu_settings = new RTIMUSettings((char *)"/usr/share/mpu9250/RTIMULib.ini");
 	imu_imu = RTIMU::createIMU(imu_settings);
 	if(!imu_imu)
 	{
-		printf("Cannot create IMU\n");
+		fprintf(stderr,"Cannot create IMU\n");
 		exit(1);
 	}
 	imu_imu->setAccelEnable(true);
 	imu_imu->setGyroEnable(true);
 	imu_imu->setCompassEnable(false);
-	
+
 	if(!imu_imu->IMUInit())
 	{
-		printf("Cannot initialize IMU\n");
+		fprintf(stderr,"Cannot initialize IMU\n");
 		exit(1);
 	}
-	
+
 	// Config and launch grab thread
 	pthread_attr_init(&attr);
 	int newprio = 20;
@@ -50,9 +50,9 @@ bool IMU::imu_init()
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
 	pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
-	
+
 	pthread_create(&threadIMU, &attr, thread_IMU, this);
-	
+
 	return true;
 }
 
@@ -112,22 +112,22 @@ void IMU::readData()
 void IMU::computePose()
 {
 	pthread_mutex_trylock(&m_mutexIMU);
-	
+
 	double pose[3] 	= {0.0, 0.0, 0.0};
 	double acc[3] 	= {0.0, 0.0, 0.0};
 	double gyr[3] 	= {0.0, 0.0, 0.0};
 	double mag[3] 	= {0.0, 0.0, 0.0};
 	int warnings[2] = {0, 0};
-	
+
 	imu_data	= imu_imu->getIMUData();
 	RTVector3 v = imu_imu->getMeasuredPose();
 	RTVector3 a = imu_imu->getAccel();
 	RTVector3 g = imu_imu->getGyro();
 	RTVector3 m = imu_imu->getCompass();
-	
-	acc[0] = a.x(); acc[1] = a.y(); acc[2] = a.z(); 
-	gyr[0] = g.x(); gyr[1] = g.y(); gyr[2] = g.z(); 
-	mag[0] = m.x(); mag[1] = m.y(); mag[2] = m.z(); 
+
+	acc[0] = a.x(); acc[1] = a.y(); acc[2] = a.z();
+	gyr[0] = g.x(); gyr[1] = g.y(); gyr[2] = g.z();
+	mag[0] = m.x(); mag[1] = m.y(); mag[2] = m.z();
 
 	// Converting rad to degree
 	pose[0] = 180.0 * v.x() / _PI_;
@@ -138,24 +138,24 @@ void IMU::computePose()
 		pose[0] = -(pose[0] -180.0);
 	else
 		pose[0] = -(pose[0] + 180.0);
-	
+
 	// Warning Analisis
 	if( abs(pose[0]) > getStrongWarningLvl() )
 		warnings[0] = 2;
 	else if (abs(pose[0]) > getLowWarningLvl())
 		warnings[0] = 1;
-		
+
 	if( abs(pose[1]) > getStrongWarningLvl() )
 		warnings[1] = 2;
 	else if (abs(pose[1]) > getLowWarningLvl())
 		warnings[1] = 1;
-	
-	printf("{p:{x:%f, y:%f, z:%f}, a:{x:%f, y:%f, z:%f}, g:{x:%f, y:%f, z:%f}, m:{x:%f, y:%f, z:%f}, w:{x:%d, y:%d}}\n", 
+
+	printf("{\"p\":{\"x\":%f,\"y\":%f,\"z\":%f},\"a\":{\"x\":%f,\"y\":%f,\"z\":%f},\"g\":{\"x\":%f,\"y\":%f,\"z\":%f},\"m\":{\"x\":%f, \"y\":%f,\"z\":%f},\"w\":{\"x\":%d,\"y\":%d}}\n",
 		pose[0], pose[1], pose[2],
 		acc[0], acc[1], acc[2],
 		gyr[0], gyr[1], gyr[2],
 		mag[0], mag[1], mag[2],
 		warnings[0], warnings[1]);
-	
+
 	pthread_mutex_unlock(&m_mutexIMU);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,46 +13,46 @@ int main(int argc, char *argv[])
 	int low_warning = 20;
 	int strong_warning = 45;
 	char *cmd = NULL;
-	
+
 	// parsing command options
 	while ((opt = getopt(argc, argv, "d:l:h:")) != -1) {
 		switch(opt) {
-			
+
 			case 'd':
 			cmd = optarg;
-			printf("Delay set to %s usec\n", cmd);
+			fprintf(stderr,"Delay set to %s usec\n", cmd);
 			delay = atoi(cmd);
 			break;
-			
+
 			case 'l':
 			cmd = optarg;
-			printf("Low Warning set to +/-%s deg\n", cmd);
+			fprintf(stderr,"Low Warning set to +/-%s deg\n", cmd);
 			low_warning = atoi(cmd);
 			break;
-			
+
 			case 'h':
 			cmd = optarg;
-			printf("Strong Warning set to +/-%s deg\n", cmd);
+			fprintf(stderr,"Strong Warning set to +/-%s deg\n", cmd);
 			strong_warning = atoi(cmd);
 			break;
-			
+
 			case '?':
-			
+
 			break;
 		}
 	}
-	
+
 	// Setting IMU Parameters
 	IMU *imu = new IMU();
 	imu->setReadTempo(delay);
 	imu->setLowWarningLvl(low_warning);
 	imu->setStrongWarningLvl(strong_warning);
-	
+
 	while(1)
 	{
 		usleep(imu->getTempo());
 		imu->computePose();
 	}
-	
+
 	return 0;
 }
